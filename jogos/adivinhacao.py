092999317461cffa82236b0f40e949aaf571ffbb
BrunoOlivei <bruoli3@gmail.com>
2021-01-07 17:59:33 -0300

    Criação de um loop para receber mais de 1 tentativa do usuário. Formatação de string utilizando interpolação.

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,19 +3,29 @@ print("Bem vindo ao jogo de Advinhação!")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute_str = input("Digite o seu número: ")
+while rodada <= total_de_tentativas:
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
 
-print("Você digitou ", chute_str)
+    chute_str = input("Digite o seu número: ")
+    print("Você digitou ", chute_str)
+    chute = int(chute_str)
 
-chute = int(chute_str)
+    acertou = chute == numero_secreto
+    maior   = chute >  numero_secreto
+    menor   = chute <  numero_secreto
 
-if numero_secreto == chute:
-    print("Você acertou")
-else:
-    if chute > numero_secreto:
-        print("Você errou! O seu chute foi maior que o número secreto.")
-    elif chute < numero_secreto:
-        print("Você errou! O seu chute foi menor que o número secreto.")
+    if acertou:
+        print("Você acertou")
+    else:
+        if maior:
+            print("Você errou! O seu chute foi MAIOR que o número secreto.")
+        elif menor:
+            print("Você errou! O seu chute foi MENOR que o número secreto.")
+
+    rodada = rodada + 1
+
 
 print("Fim do jogo")
---
+++ jogos/testes.py
@@ -0,0 +1,6 @@
+total_de_tentativas = 3
+rodada = 1
+
+while rodada <= total_de_tentativas:
+    chute_str = input("Digite o seu numero: ")
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
