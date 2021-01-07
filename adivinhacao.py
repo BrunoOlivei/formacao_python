ed3f681847823f0f711c3a7aa6f1c465124a5430
BrunoOlivei <bruoli3@gmail.com>
2021-01-06 22:31:37 -0300

    Primeiro rascunho de código do jogo de advinhação criado nas aulas de formação python da Alura.

---
+++ adivinhacao.py
@@ -0,0 +1,21 @@
+print("********************************")
+print("Bem vindo ao jogo de Advinhação!")
+print("********************************")
+
+numero_secreto = 42
+
+chute_str = input("Digite o seu número: ")
+
+print("Você digitou ", chute_str)
+
+chute = int(chute_str)
+
+if numero_secreto == chute:
+    print("Você acertou")
+else:
+    if chute > numero_secreto:
+        print("Você errou! O seu chute foi maior que o número secreto.")
+    elif chute < numero_secreto:
+        print("Você errou! O seu chute foi menor que o número secreto.")
+
+print("Fim do jogo")
