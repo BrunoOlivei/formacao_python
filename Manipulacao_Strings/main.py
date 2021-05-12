0561c30612fa5baf3970f57aa0f274ac2e26830b
BrunoOlivei <bruoli3@gmail.com>
2021-05-12 15:48:17 -0300

    Criação de classe que recebe uma url, trata, e extrai os valores conforme parâmetros solicitados.

--- Manipulacao_Strings/main.py
+++ Manipulacao_Strings/main.py
@@ -1,23 +1,33 @@
-from ExtratorArgumentosUrl import ExtratorArugmentosUrl
-import re
-'''
-url = "https://bytebank.com/cambio?moedaorigem=real&moedadestino=dolar&valor=1500"
+url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
 
-argumentos_url = ExtratorArugmentosUrl(url)
-moeda_origem, moeda_destino = argumentos_url.retorna_moedas()
-valor = argumentos_url.extrai_valor()
-print(moeda_origem, moeda_destino, valor)
-'''
+# Sanitização da URL
+url = url.strip()
 
-# Expressões Regulares
-# Utilizando exemplo de um texto contendo um número de celular
+# Validação da URL
+if url == "":
+    raise ValueError("A URL está vazia!")
 
-texto1 = "Bom dia, gostaria de agendar uma consulta, Bruno Oliveira, 99850-8547"
-texto2 = "Boa tarde, sou Bruno, 99850-8547, quero agendar uma consulta."
-texto3 = "99850-8547 Digitou: Olá sou Bruno, gostaria de agendar uma consulta."
-texto4 = "99850-8547"
+# Buscar parâmetros
+indice_interrogacao = url.find("?")  # Encontra o indice do ? e armazena a variável
+url_base = url[:indice_interrogacao]  # Fatia a url do começo até o ? exclusivo
+url_parametros = url[indice_interrogacao+1:]  # Fatia a url a partir do caractere após o ? até o final
+print(url_base)  # retorna a url base (bytebank.com/cambio)
+print(url_parametros)  # retorna os parâmetros passados a URL (quantidade, moeda destino, moeda origem etc.)
 
-padrao_celulares = "[0-9]{4,5}[-][0-9]{4}"
+# Separar os parâmetros e buscar seus valores
 
-telefone = re.search(padrao_celulares, texto1)
-print(telefone.group())
+parametro_busca = "quantidade"
+indice_parametro = url_parametros.find(parametro_busca)  # Encontra o indice do parâmetro de busca dentro da URL
+indice_valor = indice_parametro + len(parametro_busca) + 1  # Encontra o indice do valor a partir do indice do
+# parâmetro somado ao tamanho dos carácteres do parrots de busca, somado a 1 para indicar o indice do valor do
+# parâmetro
+indice_e_comercial = url_parametros.find("&", indice_valor)  # Encontra o indice do "&" a partir do indice do valor
+# solicitado
+
+# Verificar se consta na URL o & comercial para evitar erros
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
