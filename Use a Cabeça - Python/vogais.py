29b55db8b32b33bfd9947d4b86a79f8299369c87
BrunoOlivei <bruoli3@gmail.com>
2021-05-21 11:30:29 -0300

    Expressões regulares introdução

--- Use a Cabeça - Python/vogais.py
+++ Use a Cabeça - Python/vogais.py
@@ -1,5 +1,11 @@
-vogais = ['a', 'e', 'i', 'o', 'u']
-palavra = "Bolsonaro Genocida"
-for letra in palavra:
-    if letra in vogais:
-        print(letra)
+
+def encontra_vogais(palavra):
+    palavra = input("Digite uma palavra que você deseja separar as vogais:")
+    vogais = ['a', 'e', 'i', 'o', 'u']
+    letras_encontradas = []
+    for letra in palavra:
+        if letra in vogais:
+            if letra not in letras_encontradas:
+                letras_encontradas.append(letra)
+    for vogal in letras_encontradas:
+        return print(vogal)
