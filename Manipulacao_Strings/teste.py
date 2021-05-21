97518fc80fde834b2568258fcb84895ab6af6120
BrunoOlivei <bruoli3@gmail.com>
2021-05-21 13:05:13 -0300

    Testando o extrator url e extraindo a quantidade.

---
+++ Manipulacao_Strings/teste.py
@@ -0,0 +1,11 @@
+# importando a classe ExtratorURL
+from extrator_url import ExtratorUrl
+
+# URL a ser utilizada pela classe
+url = "bytebank.com/cambio?quantidade=100&moedaDestino=dolar&moedaOrigem=real"
+# Criando um objeto com a url
+extrair_url = ExtratorUrl(url)
+# Definindo uma variável que receberá o resultado do método que extrai o valor (quantidade) de dentro da url
+valor_quantidade = extrair_url.get_valor_parametro("quantidade")
+# Imprimindo o resultado
+print(valor_quantidade)
