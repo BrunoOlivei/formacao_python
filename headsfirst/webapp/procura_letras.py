9d2319393fe773946e592974e58203bc61d768ee
BrunoOlivei <bruoli3@gmail.com>
2021-06-29 18:50:11 -0300

    Página dos resultados do webapp

--- headsfirst/webapp/procura_letras.py
+++ headsfirst/webapp/procura_letras.py
@@ -1,19 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from buscavogal import procura_letras
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello world from Flask!'
-
-
 @app.route('/procuraletra', methods=['POST'])
 def faz_pesquisa() -> str:
-    return str(procura_letras('Life, the universe and everything', 'eiru'))
+    frase = request.form['frase']
+    letras = request.form['letras']
+    title = 'Aqui está o resultado:'
+    resultado = str(procura_letras(frase, letras))
+    return render_template('result.html', the_phrase=frase, the_letters=letras, the_result=resultado, the_title=title,)
 
 
+@app.route('/')
 @app.route('/entry')
 def pagina_inicial() -> 'html':
     return render_template('entry.html', the_title="Bem vindo ao procure letras!")
