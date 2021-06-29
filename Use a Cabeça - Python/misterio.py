f8b61532fb8c8c948ba1d9646ff8ba4d4768579f
BrunoOlivei <bruoli3@gmail.com>
2021-06-29 15:56:27 -0300

    Atualização sobre collections, introdução a funções Use a Cabeça e publicação de pacote de distribuição

---
+++ Use a Cabeça - Python/misterio.py
@@ -0,0 +1,11 @@
+def double(arg):
+    print("Antes: ", arg)
+    arg = arg * 2
+    print("Depois: ", arg)
+
+
+def change(arg):
+    print("Antes: ", arg)
+    arg.append("More Data")
+    print("Depois: ", arg)
+
